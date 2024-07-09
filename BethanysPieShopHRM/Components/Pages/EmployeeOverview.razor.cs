4a4b163a8773ee39770663f85d5bd39111d708d2
CiCi289 <cihtinaung289.cha@gmail.com>
2024-07-09 10:19:10 +0700

    "Binded with Domain models in separate project, Created service for Mock Data for now, Created Employee Overview page"

---
+++ BethanysPieShopHRM/Components/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,17 @@
+﻿using BethanysPieShopHRM.Services;
+using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+using System.Runtime.CompilerServices;
+
+namespace BethanysPieShopHRM.Components.Pages
+{
+  public partial class EmployeeOverview
+  {
+    public List<Employee> Employees { get; set; } = default!; //better than default! may be? = new List<Employee>();
+    protected async override Task OnInitializedAsync()
+    {
+      await Task.Delay(1000);
+      Employees = MockDataService.Employees;
+    }
+  }
+}
